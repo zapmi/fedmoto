357d251bf396bd1e190cc7bddd6427997ab0785d
Mike Z <mzapata2991@gmail.com>
2019-11-12 19:42:26 -0600

    added new nav bar

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -12,6 +12,43 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
+
+
+
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="d-flex flex-grow-1">
+            <span class="w-100 d-lg-none d-block">
+                </span>
+            <a class="navbar-brand d-none d-lg-inline-block" href="#">
+                <h2>Mike Zapata</h2>
+            </a>
+
+            <div class="w-100 text-right">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+            <ul class="navbar-nav ml-auto flex-nowrap">
+                <li class="nav-item">
+                    <a href="index.html" class="nav-link m-2 menu-item nav-active">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="portfolio.html" class="nav-link m-2 menu-item">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.html" class="nav-link m-2 menu-item">Contact</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+
+
+
+
           <div>
             <div id="bigSuzi"> <img src={bigSuzi} class="img-fluid" width="1070" height="713.73" />
             </div>
