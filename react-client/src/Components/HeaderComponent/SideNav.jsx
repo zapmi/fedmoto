524c1b0ce8ef0f3374acda7449a0059c845a1bfe
Mike Z <mzapata2991@gmail.com>
2019-11-13 23:44:39 -0600

    edited nav bar

--- react-client/src/Components/HeaderComponent/SideNav.jsx
+++ react-client/src/Components/HeaderComponent/SideNav.jsx
@@ -3,74 +3,95 @@ import { Link } from 'react-router-dom';
 class SideNav extends Component {
     render() {
         return (
-            // <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            //     <div class="d-flex flex-grow-1">
-            //         <span class="w-100 d-lg-none d-block">
-            //         </span>
-            //         <a class="navbar-brand d-none d-lg-inline-block" href="#">
-            //             <h2>FED MOTO</h2>
-            //         </a>
+            // <nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-left">
+            //     <a class="navbar-brand" href="">Navbar</a>
+            //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            //         <span class="navbar-toggler-icon"></span>
+            //     </button>
 
-            //         <div class="w-100 text-right">
-            //             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
-            //                 <span class="navbar-toggler-icon"></span>
-            //             </button>
-            //         </div>
-            //     </div>
-            //     <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
-            //         <ul class="navbar-nav ml-auto flex-nowrap">
+            //     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            //         <ul class="navbar-nav">
             //             <li class="nav-item">
-            //                 <a href="doItYourself.html" class="nav-link m-2 menu-item nav-active">DO IT YOURSELF</a>
+            //                 <a class="nav-link">Home</a>
             //             </li>
             //             <li class="nav-item">
-            //                 <a href="bikes.html" class="nav-link m-2 menu-item">BIKES</a>
+            //                 <a class="nav-link">About</a>
             //             </li>
             //             <li class="nav-item">
-            //                 <a href="shop.html" class="nav-link m-2 menu-item">SHOP +</a>
+            //                 <a class="nav-link">Contact</a>
             //             </li>
-            //             <li class="nav-item">
-            //                 <a href="service.html" class="nav-link m-2 menu-item">SERVICE</a>
+            //             <li class="nav-item dropdown">
+            //                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            //                 <div class="dropdown-menu">
+            //                     <a class="dropdown-item">Action</a>
+            //                     <a class="dropdown-item">Another action</a>
+            //                     <a class="dropdown-item">Something else here</a>
+            //                     <div class="dropdown-divider"></div>
+            //                     <a class="dropdown-item">Separated link</a>
+            //                     <a class="dropdown-item">One more separated link</a>
+            //                 </div>
             //             </li>
-            //             <li class="nav-item">
-            //                 <a href="storage.html" class="nav-link m-2 menu-item">STORAGE</a>
-            //             </li>
-            //             <li class="nav-item">
-            //                 <a href="events.html" class="nav-link m-2 menu-item">EVENTS</a>
-            //             </li>
-            //             <li class="nav-item">
-            //                 <a href="media.html" class="nav-link m-2 menu-item">MEDIA</a>
-            //             </li>
-            //             <li class="nav-item">
-            //                 <a href="careers.html" class="nav-link m-2 menu-item">CAREERS</a>
-            //             </li>
-            //             <li class="nav-item">
-            //                 <a href="signin.html" class="nav-link m-2 menu-item">SIGN IN</a>
-            //             </li>
-            //             <li class="nav-item">
-            //                 <a href="https://www.instagram.com/federalmoto/" target="_blank" class="nav-link m-2 menu-item">INSTAGRAM</a>
-            //             </li>
-            //             <li class="nav-item">
-            //                 <a href="contact.html" class="nav-link m-2 menu-item">CONTACT</a>
-            //             </li>
-
             //         </ul>
+
             //     </div>
-            // </nav>
+            // </nav >
+            <nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-left">
+            {/* <nav class="navbar.fixed-right navbar-md  navbar-light bg-light"> */}
+                <div class="d-flex flex-grow-1">
+                    <span class="w-100 d-lg-none d-block">
+                    </span>
+                    <a class="navbar-brand d-none d-lg-inline-block" href="#">
+                        <h2>FED MOTO</h2>
+                    </a>
+
+                    <div class="w-100 text-right">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                </div>
+                <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+                    <ul class="navbar-nav ml-auto flex-nowrap">
+                        <li class="nav-item">
+                            <a href="doItYourself.html" class="nav-link m-2 menu-item nav-active">DO IT YOURSELF</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="bikes.html" class="nav-link m-2 menu-item">BIKES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="shop.html" class="nav-link m-2 menu-item">SHOP +</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="service.html" class="nav-link m-2 menu-item">SERVICE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="storage.html" class="nav-link m-2 menu-item">STORAGE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="events.html" class="nav-link m-2 menu-item">EVENTS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="media.html" class="nav-link m-2 menu-item">MEDIA</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="careers.html" class="nav-link m-2 menu-item">CAREERS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="signin.html" class="nav-link m-2 menu-item">SIGN IN</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://www.instagram.com/federalmoto/" target="_blank" class="nav-link m-2 menu-item">INSTAGRAM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="contact.html" class="nav-link m-2 menu-item">CONTACT</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
 
 
-            <nav class="navbar bg-light">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link 1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link 2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link 3</a>
-                    </li>
-                </ul>
-            </nav >
+
 
 
 
