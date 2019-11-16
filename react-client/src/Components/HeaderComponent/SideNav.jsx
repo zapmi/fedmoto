4ce75e3b0dce78067eaeb5f690ed84b42d101c1e
Mike Z <mzapata2991@gmail.com>
2019-11-16 13:35:56 -0600

    side bar is now fixed and content flows

--- react-client/src/Components/HeaderComponent/SideNav.jsx
+++ react-client/src/Components/HeaderComponent/SideNav.jsx
@@ -7,8 +7,9 @@ class SideNav extends Component {
 
             <div class="row">
                 <div class="col-sm-2">
-                    <div id="slide-out" class="side-nav sn-bg-2 fixed" >
-                        <ul class="custom-scrollbar">
+                    {/* <div class="affix"> */}
+                        {/* <div id="slide-out" class="side-nav sn-bg-4 fixed" > */}
+                        <ul>
                             <li class="nav-item-logo">
                                 <div>
                                     <a href="#" ><img src="https://images.squarespace-cdn.com/content/539b5adbe4b0dc27b961c927/1568922341756-C62F6BQPUW065CIQ69WN/federallogo.jpg?content-type=image%2Fjpeg" id="fedlogo" class="img-fluid flex-center"></img></a>
@@ -44,6 +45,7 @@ class SideNav extends Component {
                             <li class="nav-item">
                                 <a href="signin.html" class="nav-link m-2 menu-item">SIGN IN</a>
                             </li>
+                            {/* <br></br>
                             <br></br>
                             <br></br>
                             <br></br>
@@ -62,22 +64,22 @@ class SideNav extends Component {
                             <br></br>
                             <br></br>
                             <br></br>
-                            <br></br>
-                            
 
-                            
+
+
                             <li class="nav-item">
                                 <a href="https://www.instagram.com/federalmoto/" target="_blank" class="nav-link m-2 menu-item">INSTAGRAM</a>
                             </li>
                             <li class="nav-item">
                                 <a href="contact.html" class="nav-link menu-item">CONTACT</a>
-                            </li>
+                            </li> */}
                         </ul>
-                    </div>
+                        {/* </div> */}
+                    {/* </div> */}
                 </div>
 
 
-                <div class="col-10" id="bigSuzipic">
+                <div class="col-sm-10" id="bigSuzipic">
 
                     <img src={bigSuzi} class="img-fluid" width="1070" height="auto" alt="bigSuzi" />
                     <img src={bigSuzi} class="img-fluid" width="1070" height="auto" alt="bigSuzi" />
