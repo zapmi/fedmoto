bb4f61a01bffa283ffcb9aca7c6ea88d85b44b6c
zapmi <mzapata2991@gmail.com>
2019-11-19 15:18:12 -0600

    added motto underneath button and styled more

--- react-client/src/Components/HomePage/HomePage.jsx
+++ react-client/src/Components/HomePage/HomePage.jsx
@@ -13,6 +13,11 @@ class HomePage extends Component {
                 <div>
                     <button class="bigSuziButton">CHECK OUT FED-014 "BIG SUZIE"</button>
                 </div>
+                <hr></hr>
+                <div id="fedMotto">
+                    Federal Moto makes one-off custom bikes and functional goods that are inspired and ridden from the city streets to the mountain peaks. Based in Chicago, Illinois.
+                </div>
+                <hr></hr>
 
                 <img src={bigSuzi} class="img-fluid" width="1309" height="auto" alt="bigSuzi" />
 
