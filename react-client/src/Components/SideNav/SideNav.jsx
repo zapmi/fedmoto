3363d5ce13129c07092056fb5237dd4d18f3fe18
Mike Z <mzapata2991@gmail.com>
2020-10-19 18:03:38 -0500

    updated side nav

--- react-client/src/Components/SideNav/SideNav.jsx
+++ react-client/src/Components/SideNav/SideNav.jsx
@@ -10,9 +10,7 @@ class SideNav extends Component {
                         <a href="#" ><img src="https://images.squarespace-cdn.com/content/539b5adbe4b0dc27b961c927/1568922341756-C62F6BQPUW065CIQ69WN/federallogo.jpg?content-type=image%2Fjpeg" id="fedlogo" class="img-fluid flex-center"></img></a>
                     </div>
                     <ul id="topSideNav">
-                        <li class="nav-item">
-                            <a href="doItYourself.html" class="nav-link m-2 menu-item nav-active">DO IT YOURSELF</a>
-                        </li>
+        
                         <li class="nav-item">
                             <a href="bikes.html" class="nav-link m-2 menu-item">BIKES</a>
                         </li>
@@ -20,26 +18,21 @@ class SideNav extends Component {
                             <a href="buildInquiry.html" class="nav-link m-2 menu-item">BUILD INQUIRY</a>
                         </li>
                         <li class="nav-item">
-                            <a href="shop.html" class="nav-link m-2 menu-item">SHOP +</a>
+                            <a href="serviceAppointment.html" class="nav-link m-2 menu-item">SERVICE APPOINTMENT</a>
                         </li>
                         <li class="nav-item">
-                            <a href="service.html" class="nav-link m-2 menu-item">SERVICE</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="storage.html" class="nav-link m-2 menu-item">STORAGE</a>
+                            <a href="shop.html" class="nav-link m-2 menu-item">SHOP</a>
                         </li>
                         <li class="nav-item">
                             <a href="events.html" class="nav-link m-2 menu-item">EVENTS</a>
                         </li>
                         <li class="nav-item">
-                            <a href="media.html" class="nav-link m-2 menu-item">MEDIA</a>
-                        </li>
-                        <li class="nav-item">
                             <a href="careers.html" class="nav-link m-2 menu-item">CAREERS</a>
                         </li>
                         <li class="nav-item">
-                            <a href="signin.html" class="nav-link m-2 menu-item">SIGN IN</a>
+                            <a href="signIn.html" class="nav-link m-2 menu-item">SIGN IN</a>
                         </li>
+                       
                     </ul>
 
                     <div class="fixed-bottom">
