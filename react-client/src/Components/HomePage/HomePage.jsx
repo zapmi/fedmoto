8e3349003078f5ef66601cbfbfdbf1076b537f58
zapmi <mzapata2991@gmail.com>
2020-10-21 13:24:29 -0500

    resized main image, added div to lower images

--- react-client/src/Components/HomePage/HomePage.jsx
+++ react-client/src/Components/HomePage/HomePage.jsx
@@ -1,45 +1,46 @@
-import React, { Component } from 'react';
-import "./HomePage.css"
-import { Link } from 'react-router-dom';
-import trashHand from '../Images/trashHand.jpg';
-import aboutFed from '../Images/aboutFed.jpg';
-import bikes from '../Images/bikes.jpg';
-import ig from '../Images/ig.jpg';
-import service from '../Images/service.png';
+import React, { Component } from "react";
+import "./HomePage.css";
+import { Link } from "react-router-dom";
+import trashHand from "../Images/trashHand.jpg";
+import aboutFed from "../Images/aboutFed.jpg";
+import bikes from "../Images/bikes.jpg";
+import ig from "../Images/ig.jpg";
+import service from "../Images/service.png";
 
 class HomePage extends Component {
-    render() {
+  render() {
+    return (
+      <div>
+        <div class="col-sm-10">
+          <a href="https://www.instagram.com/sphynxdaddy" target="_blank">
+            {" "}
+            <div class="wintStorage ">CLICK HERE TO BOOK YOUR WINTER STORAGE</div>
+          </a>
 
-        return (
-            <div class="col-sm-10">
+          <img src={trashHand} class="img-fluid" id="trashHand" alt="trashHand" />
+          <div>
+            <button class="bigSuziButton">CHECK OUT FED-014 "BIG SUZIE"</button>
+          </div>
 
-                <a href="https://www.instagram.com/sphynxdaddy" target="_blank"> <div class="wintStorage ">
-                    CLICK HERE TO BUILD YOUR WINTER STORAGE
-
+          <div id="fedMotto">
+            Federal Moto makes one-off custom bikes and functional goods that are
+            inspired and ridden from the city streets to the mountain peaks. Based
+            in Chicago, Illinois.
         </div>
-                </a>
-
-                <img src={trashHand} class="img-fluid" id="trashHand" alt="bigSuzi" />
-                <div>
-                    <button class="bigSuziButton">CHECK OUT FED-014 "BIG SUZIE"</button>
-                </div>
-                <hr></hr>
-                <div id="fedMotto">
-                    Federal Moto makes one-off custom bikes and functional goods that are inspired and ridden from the city streets to the mountain peaks. Based in Chicago, Illinois.
-                </div>
-                <hr></hr>
-
-                <img src={bikes} class="img-fluid" id="bikesPic" alt="bikes" />
-                <img src={ig} class="img-fluid" id="igPic" alt="ig" />
-                <img src={service} class="img-fluid" id="servicePic" alt="service" />
-                <img src={aboutFed} class="img-fluid" id="aboutFedPic" alt="about" />
-
-            </div>
+          <hr></hr>
+          <hr></hr>
+        </div>
 
 
+        <div class="col-sm-10">
+          <img src={bikes} class="img-fluid" id="bikesPic" alt="bikes" />
+          <img src={ig} class="img-fluid" id="igPic" alt="ig" />
+          <img src={service} class="img-fluid" id="servicePic" alt="service" />
+          <img src={aboutFed} class="img-fluid" id="aboutFedPic" alt="about" />
+        </div>
+      </div>
 
-
-        )
-    }
+    );
+  }
 }
 export default HomePage;
